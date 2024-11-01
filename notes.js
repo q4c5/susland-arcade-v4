ce4cb6cb73a6064300ac4c1c59d08e14c54d1f9c
Glitch (glitch-hello-website) <none>
2024-11-01 22:23:52 +0000

    💴⏳ Checkpoint ./madstuntcar.html:70570965/1649 ./games/index.html:70570965/569 ./games/madstuntcar.html:70570965/34 ./apps/index.html:70570965/10494 ./notes.js:70570965/274

--- notes.js
+++ notes.js
@@ -1,6 +1,6 @@
 //for the sake of susland arcade i had to add this
 //THIS IS THE EDITORS CHAT// IDC
-//
+// if ur reading this u r the rizzler of gyatt ohio fries in the bag ipad kid skibidi trump ding dong eat it up fanum tax lunchly i like my cheese drippy bruh ahh build you ate my mclaren ahh rizz u have no job no wife no family u live on the streets
 //
 //
 //
